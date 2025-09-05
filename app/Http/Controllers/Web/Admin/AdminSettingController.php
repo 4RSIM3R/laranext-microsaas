3e2f1ee82186171e957a898d074908f354526e8a
Muhammad Ilzam Mulkhaq <ilzammulkhaq85@gmail.com>
2025-09-05 22:48:02 +0700

    chore: add more shadcn component

--- app/Http/Controllers/Web/Admin/AdminSettingController.php
+++ app/Http/Controllers/Web/Admin/AdminSettingController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Web\Admin;
 use App\Contract\Setting\SystemSettingContract;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\SettingRequest;
+use App\Utils\WebResponse;
 use Inertia\Inertia;
 
 class AdminSettingController extends Controller
@@ -21,40 +22,9 @@ class AdminSettingController extends Controller
         return Inertia::render('admin/setting/index');
     }
 
-    public function fetch() {}
-
-    public function show($id)
-    {
-        $setting = $this->service->find($id);
-        return Inertia::render('admin/setting/form', [
-            'setting' => $setting
-        ]);
-    }
-
-    public function create()
-    {
-        return Inertia::render('admin/setting/form');
-    }
-
     public function store(SettingRequest $request)
     {
-        $setting = $this->service->create($request->validated());
-        return Inertia::render('admin/setting/form', [
-            'setting' => $setting
-        ]);
-    }
-
-    public function update(SettingRequest $request, $id)
-    {
-        $setting = $this->service->update($id, $request->validated());
-        return Inertia::render('admin/setting/form', [
-            'setting' => $setting
-        ]);
-    }
-
-    public function destroy($id)
-    {
-        $this->service->destroy($id);
-        return redirect()->route('admin.setting.index')->with('success', 'Setting deleted successfully');
+        $result = $this->service->create($request->validated());
+        return WebResponse::response($result);
     }
 }
